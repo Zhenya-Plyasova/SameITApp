bc240363af3a30d16563c9231c49e7a1bc382f71
Zhenya Plyasova <111362107+Zhenya-Plyasova@users.noreply.github.com>
2023-08-04 18:25:01 +0300

    initial commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,24 @@
+import manyUsers from "../img/many-users.jpg";
+
+const Home = () => {
+    return (
+      <>
+        <div
+          style={{
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+            justifyContent: 'space-between',
+            marginBottom: '100px',
+          }}
+        >
+          <h1>
+            Welcome to the APP!
+          </h1>
+          <img src={manyUsers} with = 'auto' height='450px' alt='net of people'></img>
+        </div>
+      </>
+    );
+}
+
+export default Home;
