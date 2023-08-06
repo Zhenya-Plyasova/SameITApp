e62d4e38a6d2abccbd15dfcc9b70b2369ff73769
Zhenya Plyasova <111362107+Zhenya-Plyasova@users.noreply.github.com>
2023-08-06 21:42:20 +0300

    function to get status added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,21 @@
-// import Box from '@mui/material/Box';
-// import TextField from '@mui/material/TextField';
 import CheckTTN from 'components/CheckTTN/CheckTTN';
+import { useEffect } from 'react';
+import { getTrackingStatus } from 'redux/operations';
 
 const Home = () => {
+
+  useEffect(() => {
+    const fetchTrackingData = async (number) => {
+      try {
+        const response = await getTrackingStatus(number);
+        console.log(response.data.data[0]);
+      } catch (error) {
+        console.log(error);
+      }
+    } 
+    fetchTrackingData(`20400344966669`);
+  }, [])
+
   return (
     <>
       <CheckTTN />
--- src/redux/operations.js
+++ src/redux/operations.js
@@ -1,4 +1,21 @@
 import axios from 'axios';
 // import { createAsyncThunk } from "@reduxjs/toolkit";
 
-axios.defaults.baseURL = '';
+axios.defaults.baseURL = 'https://api.novaposhta.ua/v2.0/json/';
+const apiKey = 'bc0aa8469883b07a40cd36b8052a0ae2';
+
+export function getTrackingStatus(number) {
+   const data = axios.post('/', {
+    apiKey,
+    modelName: 'TrackingDocument',
+    calledMethod: 'getStatusDocuments',
+    methodProperties: {
+      Documents: [
+        {
+          DocumentNumber: number,
+        },
+      ],
+    },
+   });
+    return data
+}
